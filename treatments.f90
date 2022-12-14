44574c8c93729d734b4a447f2619ca903acc0880
Dylan McDougall <dmcdougall@lanl.gov>
2022-12-14 15:00:29 -0700

    Added headers and license file in preparation for migration to GitHub

--- treatments.f90
+++ treatments.f90
@@ -2,6 +2,18 @@
 ! treatments contains functions which prescribe different treatments
 ! performed on the baseline forest
 !cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+! © 2022. Triad National Security, LLC. All rights reserved.  This
+! program was produced under U.S. Government contract 89233218CNA000001
+! for Los Alamos National Laboratory (LANL), which is operated by Triad
+! National Security, LLC for the U.S.  Department of Energy/National
+! Nuclear Security Administration. All rights in the program are
+! reserved by Triad National Security, LLC, and the U.S. Department of
+! Energy/National Nuclear Security Administration. The Government is
+! granted for itself and others acting on its behalf a nonexclusive,
+! paid-up, irrevocable worldwide license in this material to reproduce,
+! prepare derivative works, distribute copies to the public, perform
+! publicly and display publicly, and to permit others to do so.
+!-----------------------------------------------------------------
 subroutine treatment
 !-----------------------------------------------------------------
 ! treatment is a function which calls the appropriate treatment
